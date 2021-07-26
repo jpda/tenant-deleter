bd87131d97aca7950791fd3df44be24884fe0854
jpda <john.dandison@live.com>
2021-07-26 09:56:28 -0400

    move msal provider - should stop making new ones

---
+++ MsalTokenProvider.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+
+namespace tenant_deleter
+{
+    // todo: stop building new MsalTokenProviders for every project
+    public class MsalTokenProvider : IAuthenticationProvider
+    {
+        public readonly IPublicClientApplication _client;
+        private readonly string[] _scopes = {
+             "https://graph.microsoft.com/User.ReadWrite.All"
+            };
+
+        public MsalTokenProvider(string tenantId)
+        {
+            _client = PublicClientApplicationBuilder
+               // mt app reg in separate tenant
+               // todo: move to config
+               .Create("67d892a5-2e0d-4fb5-88d4-5e5c75d774cb")
+               .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
+               .Build();
+        }
+
+        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+        {
+            AuthenticationResult token;
+            try
+            {
+                // get an account ------ ??????
+                var account = await _client.GetAccountsAsync();
+                token = await _client.AcquireTokenSilent(_scopes, account.FirstOrDefault())
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                token = await _client.AcquireTokenWithDeviceCode(
+                    _scopes,
+                    resultCallback =>
+                    {
+                        Console.WriteLine(resultCallback.Message);
+                        return Task.CompletedTask;
+                    }
+
+                ).ExecuteAsync();
+            }
+            request.Headers.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.AccessToken);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Graph;
-using Microsoft.Identity.Client;
 
 namespace tenant_deleter
 {
@@ -27,49 +25,6 @@ namespace tenant_deleter
             await td.DeleteAllUsersFromTenant();
             Console.WriteLine("*fin*");
             Console.ReadLine();
-        }
-    }
-
-    public class MsalTokenProvider : IAuthenticationProvider
-    {
-        public readonly IPublicClientApplication _client;
-        private readonly string[] _scopes = {
-             "https://graph.microsoft.com/User.ReadWrite.All"
-            };
-
-        public MsalTokenProvider(string tenantId)
-        {
-            _client = PublicClientApplicationBuilder
-               // mt app reg in separate tenant
-               .Create("67d892a5-2e0d-4fb5-88d4-5e5c75d774cb")
-               .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
-               .Build();
-        }
-
-        public async Task AuthenticateRequestAsync(HttpRequestMessage request)
-        {
-            AuthenticationResult token;
-            try
-            {
-                // get an account ------ ??????
-                var account = await _client.GetAccountsAsync();
-                token = await _client.AcquireTokenSilent(_scopes, account.FirstOrDefault())
-                    .ExecuteAsync();
-            }
-            catch (MsalUiRequiredException ex)
-            {
-                token = await _client.AcquireTokenWithDeviceCode(
-                    _scopes,
-                    resultCallback =>
-                    {
-                        Console.WriteLine(resultCallback.Message);
-                        return Task.CompletedTask;
-                    }
-
-                ).ExecuteAsync();
-            }
-            request.Headers.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.AccessToken);
         }
     }
 
@@ -110,7 +65,7 @@ namespace tenant_deleter
                     if (currentBatchStep == users.Count)
                     {
                         _graphServiceClient.Batch.Request().PostAsync(batch).GetAwaiter().GetResult();
-                        currentBatchStep = 1; // batches are 1-indexed
+                        currentBatchStep = 1; // batches are 1-indexed, weird
                         return true;
                     }
                     currentBatchStep++;
